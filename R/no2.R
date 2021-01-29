bac39ad8cc21701da8b3da7857c09ee02985ad17
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:13:03 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 21 stazioni della regione PIEMONTE
+#'
+#' @format Un tibble con 8 colonne e 60725 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 9 stazioni della regione PIEMONTE
+#'
+#' @format Un tibble con 8 colonne e 24642 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
