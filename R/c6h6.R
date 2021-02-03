d5e08f5e42d85ef9a9f3e568569bf8a6b18c3315
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-03 21:51:01 +0100

    Integratidati mancanti del PM10, caricati dati del benzene

---
+++ R/c6h6.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di c6h6 per 9 stazioni della regione PIEMONTE
+#'
+#' @format Un tibble con 8 colonne e 24642 osservazioni
+#'
+#' @usage
+#' c6h6
+"c6h6"
---
+++ R/pm10.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm10 per 15 stazioni della regione PIEMONTE
+#'
+#' @format Un tibble con 8 colonne e 41070 osservazioni
+#'
+#' @usage
+#' pm10
+"pm10"
