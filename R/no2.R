9e910bbc8bcb783e6800c90028f5eed1e4c81393
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-18 01:56:23 +0100

    Eliminati duplicati PM10 e aggiunti inquinanti co, o3

--- R/no2.R
+++ R/no2.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di no2 per 21 stazioni della regione PIEMONTE
+#' Tibble con i dati di no2 per 17 stazioni della regione PIEMONTE
 #'
-#' @format Un tibble con 8 colonne e 60725 osservazioni
+#' @format Un tibble con 8 colonne e 46546 osservazioni
 #'
 #' @usage
 #' no2
