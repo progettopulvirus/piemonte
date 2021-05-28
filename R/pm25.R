f39ebb29fb4850cce45c4635a0844d1bf0362578
guidofioravanti <guidofioravanti@yahoo.it>
2021-05-28 07:35:01 +0200

    Aggiornamento dati

--- R/pm25.R
+++ R/pm25.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di pm25 per 15 stazioni della regione PIEMONTE
+#' Tibble con i dati di pm25 per 9 stazioni della regione PIEMONTE
 #'
-#' @format Un tibble con 8 colonne e 35783 osservazioni
+#' @format Un tibble con 8 colonne e 24642 osservazioni
 #'
 #' @usage
 #' pm25
